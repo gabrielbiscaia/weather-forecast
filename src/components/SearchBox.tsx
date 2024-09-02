64aee92b163a9ee7edb9a01871bbbd79adc02560
gabrielbiscaia <biscadev@gmail.com>
2024-09-02 17:02:47 -0300

    feat: creating navbar

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {};
+
+export default function SearchBox({}: Props) {
+  return (
+    <form className="flex relative items-center justify-center h-10">
+      <input type="text" />
+      <button></button>
+    </form>
+  );
+}
