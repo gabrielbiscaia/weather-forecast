5dd7f6ecdfdb23bd3ccc53e7f8c05af870cdbba9
gabrielbiscaia <biscadev@gmail.com>
2024-09-02 18:52:18 -0300

    feat: SearchBox props

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,17 +1,31 @@
+import cn from "@/utils/cn";
 import React from "react";
 import { IoSearch } from "react-icons/io5";
 
-type Props = {};
+type Props = {
+  className?: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+  onSubmit: React.FormEventHandler<HTMLFormElement> | undefined;
+};
 
-export default function SearchBox({}: Props) {
+export default function SearchBox(props: Props) {
   return (
-    <form className="flex relative items-center justify-center h-10">
+    <form
+      onSubmit={props.onSubmit}
+      className={cn(
+        "flex relative items-center justify-center h-10",
+        props.className,
+      )}
+    >
       <input
         type="text"
-        placeholder="Search location..."
-        className="px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:outline-none focus:border-blue-500 h-full"
+        value={props.value}
+        onChange={props.onChange}
+        placeholder="Search location.."
+        className="px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:outline-none  focus:border-blue-500 h-full"
       />
-      <button className="px-4 py-2 bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-600 whitespace-nowrap h-full">
+      <button className="px-4 py-[9px] bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-600  h-full">
         <IoSearch />
       </button>
     </form>
