067566a5157a65029a6c54dbac194fe84cf696ac
gabrielbiscaia <biscadev@gmail.com>
2024-09-03 18:08:33 -0300

    feat: API and Data Types Configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import Image from "next/image";
+import { useAtom } from "jotai";
+import { placeAtom } from "./atom";
 
 interface WeatherDetail {
   dt: number;
@@ -57,6 +63,20 @@ interface WeatherData {
 }
 
 export default function Home() {
+  const [place, setPlace] = useAtom(placeAtom);
+
+  const { isPending, error, data } = useQuery<WeatherData>({
+    queryKey: ["repoData"],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`,
+      );
+      return data;
+    },
+  });
+
+  console.log("data", data);
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
