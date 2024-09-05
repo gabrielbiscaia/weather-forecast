41d023e848fc7ffd0042fac1b3e7496d09f19383
gabrielbiscaia <biscadev@gmail.com>
2024-09-05 10:34:23 -0300

    feat: Additional Details Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,14 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useAtom } from "jotai";
 import { placeAtom } from "./atom";
-import { format, parseISO } from "date-fns";
+import { format, fromUnixTime, parseISO } from "date-fns";
 import Container from "@/components/Container";
 import convertKelvinToCelsius from "@/utils/convertKelvinToCelsius";
 import WeatherIcon from "@/components/WeatherIcon";
 import { getDayOrLightIcon } from "@/utils/getDayOrLightIcon";
+import WeatherDetails from "@/components/WeatherDetails";
+import { metersToKilometers } from "@/utils/metersToKilometers";
+import { convertWindSpeed } from "@/utils/convertWindSpeed";
 
 interface WeatherDetail {
   dt: number;
@@ -139,8 +142,34 @@ export default function Home() {
               </div>
             </Container>
           </div>
+          <div className="flex gap-4">
+            <Container className="w-fit justify-center flex-col px-4 items-center">
+              <p className="capitalize text-center">
+                {firstData?.weather[0].description ?? ""}
+              </p>
+              <WeatherIcon
+                iconName={getDayOrLightIcon(
+                  firstData?.weather[0].icon ?? "",
+                  firstData?.dt_txt ?? "",
+                )}
+              />
+            </Container>
+            <Container className="bg-yellow-300/80 px-6 gap-4 justify-between overflow-x-auto">
+              <WeatherDetails
+                visibility={metersToKilometers(firstData?.visibility ?? 10000)}
+                humidity={`${firstData?.main.humidity}%`}
+                windSpeed={convertWindSpeed(firstData?.wind.speed ?? 1.64)}
+                airPressure={`${firstData?.main.pressure} hPa`}
+                sunrise={`${format(fromUnixTime(data?.city.sunrise ?? 1702949452), "H :mm")}`}
+                sunset={`${format(fromUnixTime(data?.city.sunset ?? 1702517657), "H:mm")}`}
+              />
+            </Container>
+          </div>
         </section>
-        <section></section>
+
+        <section className="flex w-full flex-col gap-4">
+          <p className="text-2xl">Forecast (7 days)</p>
+        </section>
       </main>
     </div>
   );
